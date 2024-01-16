ddbc24c80ca484a7897edd2f5e1b30561ea95246
Samrith Shankar <samrith.298@gmail.com>
2024-01-16 15:52:19 +0530

    Enable freeze app-wide

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+import { enableFreeze } from "react-native-screens";
+
 import { Providers } from "~/Providers";
 import RootStack from "~/router/RootStack";
+
+enableFreeze(true);
 
 export default function App() {
 	return (
